4253007d93be6a2f684261e6e9a300f19857c025
Dmitrii Goriunov <goriunovd@gmail.com>
2016-08-05 12:25:10 +1200

    New deploy

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req , res ,next) {
   res.setHeader('Access-Control-Allow-Origin' , '*');
-  res.setHeader('Access-Control-Allow-Methods' , '*');
+  res.setHeader('Access-Control-Allow-Methods' , 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers' , 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
   next();
   
 });
