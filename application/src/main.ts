d23795d6469e9311b90ce7bedf6d25ffdb4f9904
goriunovd <gotiunovd@gmail.com>
2016-09-01 19:32:51 +1200

    Logo added )

--- application/src/main.ts
+++ application/src/main.ts
@@ -1,28 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import {enableProdMode, provide} from '@angular/core';
-// import { AppComponent, environment } from './app/';
-// import {APP_ROUTER_PROVIDER} from "./app/app.route";
-// import {ContentService} from "./app/content/content.service";
-// import {provideForms, disableDeprecatedForms} from "@angular/forms";
-// import {HTTP_PROVIDERS} from "@angular/http";
-// import {LocationStrategy, HashLocationStrategy} from "@angular/common";
-// import {AuthService} from "./app/shared/auth.service";
-// import {ChatService} from "./app/chat/chat.service";
+import {enableProdMode} from '@angular/core';
+
 import {AppModule} from "./app/app.module";
 
 enableProdMode();
 
 
 platformBrowserDynamic().bootstrapModule(AppModule);
-
-// bootstrap(AppComponent , [
-//   APP_ROUTER_PROVIDER,
-//   ContentService,
-//   disableDeprecatedForms(),
-//   provideForms(),
-//   HTTP_PROVIDERS,
-//   provide(LocationStrategy, {'useClass': HashLocationStrategy }),
-//   AuthService,
-//   ChatService
-// ]);
-
